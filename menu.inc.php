0b53f926de46c896b98e49a155d09e6c8e7719e5
Scrapbooking <cyril.cortez.fr@gmail.com>
2014-03-19 16:15:04 +0100

    Create menu.inc.php

---
+++ menu.inc.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+  		<meta charset="UTF-8">
+   	<title>Menu</title>	
+	<link rel="stylesheet" href="css/screen.css" media="all">
+</head>
+<body>
+	<div class="menu">
+		<ul>
+			<li><a href="#">Accueil</a></li>
+			<li><a href="#">Créations</a></li>
+			<li><a href="#">Matériel</a></li>
+			<li><a href="#">Actualités</a></li>
+			<li><a href="#">Evènements</a></li>
+			<li><a href="#">Infos/Contact</a></li>
+		</ul>
+	</div>
+</body>
+</html> 
