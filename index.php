f1f02499072cd497f9791268920686f703f35c5c
Scrapbooking <cyril.cortez.fr@gmail.com>
2014-03-19 16:14:02 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<head>
+	<title>Index</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="">
+	<meta name="keywords" content="Scrapbooking, créations, livres, démonstratrice">		
+<!-- Feuille de réinitialisation css (Supprime les règles CSS de base) -->	
+	<link rel="stylesheet" type="text/css" href="css/screen.css" media="all">
+	<link rel="stylesheet" type="text/css" href="css/main.css" media="all">
+	<link rel="shortcut icon" href="favicon.ico">
+</head>
+<body>
+	<header>			
+		<div id="bandeau">
+			<div id="logo"></div>
+		</div>
+	</header>
+	<nav>
+		<?php include('menu.inc.php');?>
+		 </div>
+	</nav>
+	<div id="imagebandeau">
+	</div>
+	<section>
+		<div id="corps">
+		</div>
+	</section>
+	<footer> 
+	</footer>
+</body>
+</html>
