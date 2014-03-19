7eb6848929f9b8b7115ad2692d7f1069e31994b3
Scrapbooking <cyril.cortez.fr@gmail.com>
2014-03-19 17:06:52 +0100

    Update menu.inc.php

--- menu.inc.php
+++ menu.inc.php
@@ -1,12 +1,11 @@
-
-	<div class="menu">
-		<ul>
-			<li><a href="#">Accueil</a></li>
-			<li><a href="#">Créations</a></li>
-			<li><a href="#">Matériel</a></li>
-			<li><a href="#">Actualités</a></li>
-			<li><a href="#">Evènements</a></li>
-			<li><a href="#">Infos/Contact</a></li>
-		</ul>
-	</div>
+<div class="menu">
+	<ul>
+		<li><a href="#">Accueil</a></li>
+		<li><a href="#">Créations</a></li>
+		<li><a href="#">Matériel</a></li>
+		<li><a href="#">Actualités</a></li>
+		<li><a href="#">Evènements</a></li>
+		<li><a href="#">Infos/Contact</a></li>
+	</ul>
+</div>
 
